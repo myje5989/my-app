7e6829eb0271c68322f01bf69b6d8c6950e29e0b
myje5989 <30823860+myje5989@users.noreply.github.com>
2018-04-29 16:59:47 +0700

    add

--- src/ToDo.js
+++ src/ToDo.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import './ToDo.css';
-import { Container, Row, Col ,Button, InputGroupAddon ,Input, Alert, Form, FormGroup} from 'reactstrap';
+import { Container, Row, Col ,Button, InputGroupAddon ,InputGroup,Input, Alert, Form, FormGroup} from 'reactstrap';
 
 
 
 class ToDo extends Component {
+  state ={
+    task: ''
+  }
+
+  onAdd = () =>{
+    this.props.addToDo(this.state.task)
+  }
 
   render() {
 
@@ -17,15 +24,24 @@ class ToDo extends Component {
           <Row>
         <Col></Col>
           <Col>
-       <Form inline>
-        <FormGroup>
-          <Input type="text" name="textbx" id="textbx" size="lg" placeholder="Enter text !!" />
-          <InputGroupAddon addonType="append" ><Button size="lg" color="primary">+</Button></InputGroupAddon>
-        </FormGroup>
+       <Form inline >
+        <InputGroup>
+          <Input
+           size="lg" placeholder="Enter task !!"
+           value={this.state.task}
+           onChange = {(e) => this.setState({task: e.target.value })}
+           />
+          <InputGroupAddon addonType="append" >
+          <Button size="lg" color="primary">+</Button>
+          </InputGroupAddon>
+        </InputGroup>
       </Form>
       </Col>
       <Col></Col>
       </Row>
+      <br/>
+        <br/>
+        <br/>
       </Container>
 
       </div>
